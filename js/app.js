bdef0b19b4cb7ec23c2c34f992ff570a83f8fd30
luantaraschi <luantaraschi@gmail.com>
2024-10-09 18:32:14 -0300

    adicionado confirmacao ao devolver o jogo

--- js/app.js
+++ js/app.js
@@ -6,9 +6,11 @@ function alterarStatus(id) {
  elemento, seleciona três elementos filhos com classes específicas (dashboard__item__img e dashboard__item__button */
   
     if (imagem.classList.contains('dashboard__item__img--rented')) { //verificando se a imagem está alugada
+        if (confirm("Você deseja devolver o jogo?")) { // Adicionando um prompt de confirmação
         imagem.classList.remove('dashboard__item__img--rented'); //retirando a classe
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent  = 'Alugar'; //alterando o texto do botão
+        }
     } else  {
         imagem.classList.add('dashboard__item__img--rented');  //adicionando a classe
         botao.textContent  = 'Devolver'; //alterando o texto do botão
