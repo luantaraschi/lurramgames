7383cd01f5f66ee14eae3834c257490e21a522b4
luantaraschi <luantaraschi@gmail.com>
2024-10-09 16:27:04 -0300

    botoes agora sao funcionais

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function alterarStatus(id) {
+    let gameClickado = document.getElementById(`game-${id}`);
+    let imagem = gameClickado.querySelector('.dashboard__item__img'); 
+    let botao = gameClickado.querySelector('.dashboard__item__button');  
+/* A função alterarStatus seleciona um elemento HTML com um ID específico (game-${id}) e, dentro desse
+ elemento, seleciona três elementos filhos com classes específicas (dashboard__item__img e dashboard__item__button */
+  
+    if (imagem.classList.contains('dashboard__item__img--rented')) { //verificando se a imagem está alugada
+        imagem.classList.remove('dashboard__item__img--rented'); //retirando a classe
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent  = 'Alugar'; //alterando o texto do botão
+    } else  {
+        imagem.classList.add('dashboard__item__img--rented');  //adicionando a classe
+        botao.textContent  = 'Devolver'; //alterando o texto do botão
+        botao.classList.add('dashboard__item__button--return');
+    }
+
+}
